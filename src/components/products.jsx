bd47b5dcb90cbb1760e25d6275a17d3ec92ca6d8
Vinay Phatarpekar <vinayp9399@gmail.com>
2024-06-26 16:50:38 +0530

    1234

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -10,7 +10,7 @@ const Products = ()=>{
 
     const userid = localStorage.getItem('id');
     const getallproductData = ()=>{
-        axios.get(`http://https://e-comwebsite-backend.vercel.app/products/findproducts/${params.category}`).then((response)=>{
+        axios.get(`https://e-comwebsite-backend.vercel.app/products/findproducts/${params.category}`).then((response)=>{
             setproductdata(response.data.message)
         })
     }
